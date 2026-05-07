5c06905874790a8839fe1b3b8d37e6d72861ae41
Ishwar Bade <ishwarbade3@gmail.com>
2026-05-07 22:12:27 +0530

    u

---
+++ Array_Problems/src/Problem_83.java
@@ -0,0 +1,15 @@
+
+public class Problem_83 {
+//	public ListNode deleteDuplicates(ListNode head) {
+//        if(head == null )return head;
+//        ListNode temp=head;
+//         while(temp!=null && temp.next!=null){
+//            if(temp.val== temp.next.val)
+//                temp.next=temp.next.next;
+//            else 
+//            temp=temp.next; 
+//         }
+//         return head;  
+//    }
+
+}
