6e5134717da989700550fb7da4b0a255e838b111
Ishwar Bade <ishwarbade3@gmail.com>
2026-05-17 21:54:58 +0530

    u

---
+++ Array_Problems/src/Problem_203.java
@@ -0,0 +1,20 @@
+
+public class Problem_203 {
+//	public ListNode removeElements(ListNode head, int val) {
+//		ListNode dummy = new ListNode(-1);
+//		dummy.next = head;
+//
+//		ListNode temp = dummy;
+//
+//		while (temp != null && temp.next != null) {
+//
+//			if (temp.next.val == val)
+//				temp.next = temp.next.next;
+//			else
+//				temp = temp.next;
+//		}
+//
+//		return dummy.next;
+//	}
+
+}
