12dd25962abd338916a0de9fa829547381572c85
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-06 23:47:56 +0530

    u

---
+++ Leetcode_Problem/src/Problem_160.java
@@ -0,0 +1,49 @@
+
+public class Problem_160 {
+//	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//
+//		int countA = 0;
+//		int countB = 0;
+//
+//		ListNode temp1 = headA;
+//		while (temp1 != null) {
+//			countA++;
+//			temp1 = temp1.next;
+//		}
+//		ListNode temp2 = headB;
+//		while (temp2 != null) {
+//			countB++;
+//			temp2 = temp2.next;
+//		}
+//		int move = 0;
+//		int count = 0;
+//		temp2 = headB;
+//		temp1 = headA;
+//		if (countA > countB) {
+//			move = countA - countB;
+//			temp1 = headA;
+//			while (count != move) {
+//				temp1 = temp1.next;
+//				count++;
+//			}
+//		} else {
+//			move = countB - countA;
+//			temp2 = headB;
+//			while (count != move) {
+//				temp2 = temp2.next;
+//				count++;
+//			}
+//
+//		}
+//		while (temp1 != null && temp2 != null) {
+//			if (temp1 == temp2)
+//				return temp1;
+//
+//			temp1 = temp1.next;
+//			temp2 = temp2.next;
+//		}
+//		return null;
+
+	}
+
+
