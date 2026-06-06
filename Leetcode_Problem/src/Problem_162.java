74bb3d37e16bd3ae2b8c0695140d69010b5c43a9
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-07 00:27:22 +0530

    u

---
+++ Leetcode_Problem/src/Problem_162.java
@@ -0,0 +1,14 @@
+
+public class Problem_162 {
+	public int findPeakElement(int[] nums) {
+		int max = 0;
+
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] > nums[max]) {
+				max = i;
+			}
+		}
+		return max;
+
+	}
+}
