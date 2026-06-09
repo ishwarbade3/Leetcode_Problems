54dea720d323e426ecdc9338133f24f63bee338c
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-09 22:11:19 +0530

    u

---
+++ Leetcode_Problem/src/Problem_744.java
@@ -0,0 +1,13 @@
+
+public class Problem_744 {
+	public char nextGreatestLetter(char[] letters, char target) {
+
+		for (int i = 0; i < letters.length; i++) {
+			if (letters[i] > target)
+				return letters[i];
+
+		}
+		return letters[0];
+
+	}
+}
---
+++ Leetcode_Problem/src/Problem_796.java
@@ -0,0 +1,10 @@
+
+public class Problem_796 {
+	public boolean rotateString(String s, String goal) {
+		if (s.length() != goal.length())
+			return false;
+
+		return (s + s).contains(goal);
+	}
+
+}
