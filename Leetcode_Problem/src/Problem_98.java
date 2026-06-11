8d359ce700f8e469fb9046f2aae0ce2b07689774
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-11 22:51:49 +0530

    u

---
+++ Leetcode_Problem/src/Problem_98.java
@@ -0,0 +1,24 @@
+
+public class Problem_98 {
+	// TreeNode prev=null;
+							// public boolean isValidBST(TreeNode root) {
+							// return validate(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+						
+							// }
+							// public boolean validate(TreeNode root,int min , int max){
+							// if(root==null)return true;
+						
+							// if(root.val <=min ||root.val>=max)return false;
+						
+							// return validate(root.left,min,root.val)&&validate(root.right,root.val,max);
+							//
+//    if(root==null)return true;
+//    if(!isValidBST(root.left))return false;
+//    if(prev!=null && prev.val>=root.val)return false;
+//
+//    prev=root;
+//    return isValidBST(root.right);
+
+	// }
+
+}
