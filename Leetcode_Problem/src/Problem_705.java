d8039e9cd882d9923e2cc7ccffc3ae665b21e7c3
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-12 22:58:44 +0530

    u

---
+++ Leetcode_Problem/src/Problem_705.java
@@ -0,0 +1,23 @@
+
+public class Problem_705 {
+	class MyHashSet {
+		private boolean set[];
+
+		public MyHashSet() {
+			set = new boolean[1000001];
+		}
+
+		public void add(int key) {
+			set[key] = true;
+		}
+
+		public void remove(int key) {
+			set[key] = false;
+		}
+
+		public boolean contains(int key) {
+			return set[key];
+		}
+	}
+
+}
