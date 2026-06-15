72b9a832e48b71e434dc59e17f95d2e3f28ef0ac
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-16 00:14:32 +0530

    u

---
+++ Leetcode_Problem/src/Problem_145.java
@@ -0,0 +1,25 @@
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import javax.swing.tree.TreeNode;
+
+public class Problem_145 {
+//	public List<Integer> postorderTraversal(TreeNode root) {
+//		List<Integer> list = new ArrayList<Integer>();
+//		if (root == null)
+//			return list;
+//		return postTraversal(root, list);
+//	}
+//
+//	public List<Integer> postTraversal(TreeNode root, List<Integer> list) {
+//		if (root == null)
+//			return null;
+//
+//		postTraversal(root.left, list);
+//		postTraversal(root.right, list);
+//		list.add(root.val);
+//
+//		return list;
+//	}
+
+}
