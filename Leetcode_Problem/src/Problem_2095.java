248082220083a8b238396fa2abef31fdddeaef88
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-16 00:01:25 +0530

    u

---
+++ Leetcode_Problem/src/Problem_2095.java
@@ -0,0 +1,22 @@
+
+public class Problem_2095 {
+//	 public ListNode deleteMiddle(ListNode head) {
+//	        if (head.next == null)
+//	            return null;
+//	        ListNode prev = new ListNode(-1);
+//	        prev.next = head;
+//
+//	        ListNode slow = head;
+//	        ListNode fast = head;
+//
+//	        while (fast != null && fast.next != null) {
+//	            slow = slow.next;
+//	            fast = fast.next.next;
+//	            prev = prev.next;
+//	        }
+//	        prev.next = slow.next;
+//	        return head;
+//
+//	    }
+
+}
