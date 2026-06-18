44dac410895431513a42dadb62d70fb033a0fcc5
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-18 22:48:54 +0530

    u

---
+++ Leetcode_Problem/src/Problem_112.java
@@ -0,0 +1,17 @@
+
+
+public class Problem_112 {
+//	public boolean hasPathSum(TreeNode root, int targetSum) {
+//		if (root == null)
+//			return false;
+//
+//		if (root.left == null && root.right == null)
+//			return targetSum == root.val;
+//
+//		int remaining = targetSum - root.val;
+//
+//		return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining);
+//
+//	}
+
+}
